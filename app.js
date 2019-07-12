27cb7bb619470bb8f7b33d2056a2cbb0bb292bff
Dalton Dayton <daltondayton1@gmail.com>
2019-07-12 16:44:39 -0600

    Add delete route. Add edit button

--- app.js
+++ app.js
@@ -90,6 +90,19 @@ app.put("/blogs/:id", (req, res) => {
     });
 });
 
+// DELETE ROUTE
+app.delete("/blogs/:id", (req, res) => {
+    // Destroy Blog
+    Blog.findByIdAndRemove(req.params.id, err => {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            // Redirect
+            res.redirect("/blogs");
+        }
+    });
+});
+
 app.listen(3000, function() {
     console.log("Server initiated on port 3000;");
 });
