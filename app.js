493875a7ba9e51842763a0e42557e2f39d4fe705
Dalton Dayton <daltondayton1@gmail.com>
2019-07-12 15:52:50 -0600

    Add new post page

--- app.js
+++ app.js
@@ -26,12 +26,31 @@ app.get("/", (req, res) => {
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", (req, res) => {
     Blog.find({}, (err, blogs) => {
         if (err) {
             console.log("Error!");
         } else {
             res.render("index", { blogs: blogs });
+        }
+    });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", (req, res) => {
+    // Create Blog
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            res.render("new");
+        } else {
+            // Then, Redirect to the index
+            res.redirect("/blogs");
         }
     });
 });
